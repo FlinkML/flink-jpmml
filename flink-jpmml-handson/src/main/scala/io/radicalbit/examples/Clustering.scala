9951732b4544fcc2220c9461dab1256062b5a0e9
francescofrontera <francesco.frontera@radicalbit.io>
2017-05-20 17:16:40 +0200

    Refactoring removed unused enum and imports

--- flink-jpmml-handson/src/main/scala/io/radicalbit/examples/Clustering.scala
+++ flink-jpmml-handson/src/main/scala/io/radicalbit/examples/Clustering.scala
@@ -4,23 +4,8 @@ import org.apache.flink.streaming.api.scala._
 import io.radicalbit.examples.model.IrisSource._
 import io.radicalbit.flink.pmml.scala.api.reader.ModelReader
 import io.radicalbit.flink.pmml.scala._
-import io.radicalbit.flink.pmml.scala.models.{EmptyScore, Score, Target}
 
-object IrisCategory extends Enumeration {
-  type IrisCategory = Value
-  val IrisSetosa, IrisVersicolour, IrisVirginica, NaN = Value
-}
 object Clustering {
-  import IrisCategory._
-
-  def matchingPrediction(prediction: Target): IrisCategory = {
-    prediction.getOrElse(-1.0) match {
-      case 1 => IrisSetosa
-      case 2 => IrisVersicolour
-      case 3 => IrisVirginica
-      case _ => NaN
-    }
-  }
 
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
