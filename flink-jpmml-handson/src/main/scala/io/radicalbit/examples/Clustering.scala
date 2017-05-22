118275c9ce43bbbdf2d44849743237e8216e0a73
francescofrontera <francesco.frontera@radicalbit.io>
2017-05-22 14:39:22 +0200

    Refactoring read model from args

--- flink-jpmml-handson/src/main/scala/io/radicalbit/examples/Clustering.scala
+++ flink-jpmml-handson/src/main/scala/io/radicalbit/examples/Clustering.scala
@@ -4,17 +4,22 @@ import org.apache.flink.streaming.api.scala._
 import io.radicalbit.examples.model.IrisSource._
 import io.radicalbit.flink.pmml.scala.api.reader.ModelReader
 import io.radicalbit.flink.pmml.scala._
+import org.apache.flink.api.java.utils.ParameterTool
 
 object Clustering {
 
   def main(args: Array[String]): Unit = {
+    val params: ParameterTool = ParameterTool.fromArgs(args)
     val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+    env.getConfig.setGlobalJobParameters(params)
+    val inputModel = if (params.has("model")) params.get("model") else throw new IllegalArgumentException("model are required")
 
     //Read data from custom iris source
     val irisDataStream = irisSource(env)
 
     //Load model
-    val modelReader = ModelReader("/kmeans.xml")
+    val modelReader = ModelReader(inputModel)
 
     //Using evaluate operator
     val prediction = irisDataStream.evaluate(modelReader) {
