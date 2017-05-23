8628c7963b361b1812e11957e06ba611b0f70cfd
francescofrontera <francesco.frontera@radicalbit.io>
2017-05-24 00:54:19 +0200

    Fixing handson EnsureParameter trait

--- flink-jpmml-handson/src/main/scala/io/radicalbit/examples/util/EnsureParameters.scala
+++ flink-jpmml-handson/src/main/scala/io/radicalbit/examples/util/EnsureParameters.scala
@@ -24,10 +24,8 @@ package io.radicalbit.examples.util
 import org.apache.flink.api.java.utils.ParameterTool
 
 trait EnsureParameters {
-  def ensureParams(params: ParameterTool) = {
-    (params.has("model"), params.has("output")) match {
-      case (true, true) => (params.get("model"), params.get("output"))
-      case _ => throw new IllegalArgumentException("model are required")
-    }
-  }
+
+  def ensureParams(params: ParameterTool) =
+    (params.getRequired("model"), params.getRequired("output"))
+
 }
