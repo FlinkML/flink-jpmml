3ea0d43e5d135c854135f4492a53f6be4ef51f5f
francescofrontera <francesco.frontera@radicalbit.io>
2017-05-26 23:03:17 +0200

    Correct license file

--- project/LicenseSetting.scala
+++ project/LicenseSetting.scala
@@ -43,7 +43,7 @@ object LicenseSetting {
            |GNU Affero General Public License for more details.
            |
            |You should have received a copy of the GNU Affero General Public License
-           |along with this program.  If not, see <http://www.gnu.org/licenses/>.
+           |along with flink-JPMML.  If not, see <http://www.gnu.org/licenses/>.
            |""".stripMargin
       )),
     headerMappings := headerMappings.value + (
