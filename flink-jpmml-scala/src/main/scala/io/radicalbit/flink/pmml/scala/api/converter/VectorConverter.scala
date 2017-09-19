07e6015cfb223d5f56204b0598c895cde13e426c
Francesco Frontera <francesco.frontera@radicalbit.io>
2017-09-19 10:21:15 +0200

    #22 - Avoiding boilerplate to Vector handler Type Class (#23)
    * Refactoring methods
    
    * Refactoring Vector Converter
    
    * Added ScalaDocs
    
    * Refactoring
    
    * Refactoring methods
    
    * Refactoring

--- flink-jpmml-scala/src/main/scala/io/radicalbit/flink/pmml/scala/api/converter/VectorConverter.scala
+++ flink-jpmml-scala/src/main/scala/io/radicalbit/flink/pmml/scala/api/converter/VectorConverter.scala
@@ -27,70 +27,63 @@ import scala.collection.JavaConversions._
 /** Type Class Pattern implementing converters from Flink [[org.apache.flink.ml.math.Vector]] instances to
   * internal types; the existing fields (i.e. value defined fields) are modeled as [[scala.collection.mutable.Map]]s; the
   * not existing fields (i.e. NaN values) will not be mapped within the Internal type
-  *
-  *
   */
-sealed trait VectorConverter[T] extends Serializable {
+sealed trait VectorConverter[-T] extends Serializable {
   def serializeVector(v: T, eval: Evaluator): Map[String, Any]
 }
 
 private[api] object VectorConverter {
 
-  /** Type class pattern entry-point: it deliveries right converter depending
+  private def apply[A: VectorConverter] = implicitly[VectorConverter[A]]
+
+  /**
+    * Create an instance for specific [[VectorConverter]]
+    *
+    * @param serialize The function producing a [[PmmlInput]]
+    * @return The vector converter instance
+    *
+    * */
+  private def createConverter[IN](serialize: (IN, Evaluator) => PmmlInput): VectorConverter[IN] =
+    new VectorConverter[IN] {
+      override def serializeVector(v: IN, eval: Evaluator): PmmlInput =
+        serialize(v, eval)
+    }
+
+  /** Type class pattern entry-point: it delivers right converter depending
     * on the input type (i.e. Dense or Sparse)
     *
+    * @return The specific converter instance for type [[Vector]]
+    *
     */
-  private[api] implicit object VectorConversion extends VectorConverter[Vector] {
-
-    /** Converts a [[Vector]] to the internal type by mapping PMML model fields to vector values.
-      *
-      * @param v Input vector instance as [[Vector]]
-      * @param evaluator Evaluator instance as [[Evaluator]]
-      * @return The converted instance (could be either `Dense` or `Sparse`
-      */
-    def serializeVector(v: Vector, evaluator: Evaluator): PmmlInput = v match {
-      case denseVector: DenseVector => DenseVector2Map.serializeVector(denseVector, evaluator)
-      case sparseVector: SparseVector => SparseVector2Map.serializeVector(sparseVector, evaluator)
+  private[api] implicit val vectorConversion: VectorConverter[Vector] =
+    createConverter {
+      case (vec: DenseVector, evaluator) => denseVector2Map.serializeVector(vec, evaluator)
+      case (vec: SparseVector, evaluator) => sparseVector2Map.serializeVector(vec, evaluator)
     }
 
-  }
-
-  private[api] implicit object DenseVector2Map extends VectorConverter[DenseVector] {
-
-    /** Converts a [[DenseVector]] to the internal type by mapping PMML model fields to vector values.
-      *
-      * @param v The input Dense Vector
-      * @param evaluator The evaluator instance
-      * @return The converted instance
-      */
-    def serializeVector(v: DenseVector, evaluator: Evaluator): PmmlInput = {
-      val getNameInput = getKeyFromModel(evaluator)
-
-      getNameInput.zip(v.data).toMap
-    }
-  }
-
-  private[api] implicit object SparseVector2Map extends VectorConverter[SparseVector] {
-
-    /** Converts a [[SparseVector]] to the internal type by mapping PMML model fields to vector values.
-      * Note that only existing values will be mapped
-      *
-      * @param v The input Sparse vector
-      * @param evaluator The evaluator instance
-      * @return The converted instance
-      */
-    def serializeVector(v: SparseVector, evaluator: Evaluator): PmmlInput = {
-      val getNameInput = getKeyFromModel(evaluator)
-
-      getNameInput.zip(toDenseData(v)).collect { case (key, Some(value)) => (key, value) }.toMap
+  /** Converts a [[DenseVector]] to the internal type by mapping PMML model fields to vector values.
+    *
+    * @return The converted instance
+    */
+  private[api] implicit val denseVector2Map: VectorConverter[DenseVector] =
+    createConverter { (vec, evaluator) =>
+      getKeyFromModel(evaluator)
+        .zip(vec.data)
+        .toMap
     }
 
-    private def toDenseData(sparseVector: SparseVector): Seq[Option[Any]] =
-      (0 to sparseVector.size).map { index =>
-        if (sparseVector.indices.contains(index)) Some(sparseVector(index)) else None
-      }
-
-  }
+  /** Converts a [[SparseVector]] to the internal type by mapping PMML model fields to vector values.
+    * Note that only existing values will be mapped
+    *
+    * @return The converted instance
+    */
+  private[api] implicit val sparseVector2Map: VectorConverter[SparseVector] =
+    createConverter { (vec, evaluator) =>
+      getKeyFromModel(evaluator)
+        .zip(toDenseData(vec))
+        .collect { case (key, Some(value)) => (key, value) }
+        .toMap
+    }
 
   private[api] implicit def applyConversion[T: VectorConverter, E <: Evaluator](dataVector: T, evaluator: E) =
     implicitly[VectorConverter[T]].serializeVector(dataVector, evaluator)
@@ -103,5 +96,10 @@ private[api] object VectorConverter {
     */
   private def getKeyFromModel(evaluator: Evaluator) =
     evaluator.getActiveFields.map(_.getName.getValue)
+
+  private def toDenseData(sparseVector: SparseVector): Seq[Option[Any]] =
+    (0 until sparseVector.size).map { index =>
+      if (sparseVector.indices.contains(index)) Some(sparseVector(index)) else None
+    }
 
 }
